2c2f199a93da58c702bb6e6dd17afabddf246204
Polina <106979919+kollarbone@users.noreply.github.com>
2023-02-25 00:59:57 +0300

    some blocks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header-footer">
+        <p>header</p>
+      </header>
+      <div className="main-content">
+        <div className="block-1">
+          <div className="big-block grey block-1-first">
+            <p>
+              Гений непредвзято понимает под собой конфликт. Закон внешнего
+              мира, очевидно, реально подчеркивает аксиоматичный здравый смысл.
+              Культ джайнизма включает в себя поклонение Махавире и другим
+              тиртханкарам, поэтому закон исключённого третьего индуцирует
+              изоморфный ротор.
+            </p>
+          </div>
+          <div className="small-block grey block-1-second">
+            <p>Чувство однократно</p>
+          </div>
+          <div className="small-block grey block-1-third">
+            <p>Морено</p>
+          </div>
+        </div>
+        <div className="block-2">
+          <div className="narrow-block light block-2-first">
+            <p>1</p>
+          </div>
+          <div className="narrow-block light block-2-second">
+            <p>2</p>
+          </div>
+          <div className="narrow-block blue block-2-third">
+            <p>3</p>
+          </div>
+          <div className="wide-narrow-block light block-2-fouth">
+            <p>4</p>
+          </div>
+          <div className="narrow-block yellow block-2-fifth">
+            <p>5</p>
+          </div>
+        </div>
+        <div className="block-3">main content</div>
+        <div className="block-4">main content</div>
+      </div>
+      <header className="App-header-footer">
+        <p>footer</p>
       </header>
     </div>
   );
