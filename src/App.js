66074370f457bf9e4c33b91d7eb030d34c5ab80c
Polina <106979919+kollarbone@users.noreply.github.com>
2023-02-25 16:42:43 +0300

    mobile

--- src/App.js
+++ src/App.js
@@ -7,38 +7,40 @@ function App() {
         <p>header</p>
       </header>
       <div className="main-content">
-        <div className="block-1">
-          <div className="big-block grey block-1-first">
-            <p>
-              Гений непредвзято понимает под собой конфликт. Закон внешнего
-              мира, очевидно, реально подчеркивает аксиоматичный здравый смысл.
-              Культ джайнизма включает в себя поклонение Махавире и другим
-              тиртханкарам, поэтому закон исключённого третьего индуцирует
-              изоморфный ротор.
-            </p>
+        <div className="remove-blocks">
+          <div className="block-1">
+            <div className="big-block grey block-1-first">
+              <p>
+                Гений непредвзято понимает под собой конфликт. Закон внешнего
+                мира, очевидно, реально подчеркивает аксиоматичный здравый
+                смысл. Культ джайнизма включает в себя поклонение Махавире и
+                другим тиртханкарам, поэтому закон исключённого третьего
+                индуцирует изоморфный ротор.
+              </p>
+            </div>
+            <div className="small-block grey block-1-second">
+              <p>Чувство однократно</p>
+            </div>
+            <div className="small-block grey block-1-third">
+              <p>Морено</p>
+            </div>
           </div>
-          <div className="small-block grey block-1-second">
-            <p>Чувство однократно</p>
-          </div>
-          <div className="small-block grey block-1-third">
-            <p>Морено</p>
-          </div>
-        </div>
-        <div className="block-2">
-          <div className="narrow-block light block-2-first">
-            <p>Наши сотрудники</p>
-          </div>
-          <div className="narrow-block light block-2-second">
-            <p>Просветляет понимающий тест</p>
-          </div>
-          <div className="narrow-block blue block-2-third">
-            <p>Интроекция</p>
-          </div>
-          <div className="wide-narrow-block light block-2-fouth">
-            <p>Конформизм</p>
-          </div>
-          <div className="narrow-block yellow block-2-fifth">
-            <p>Наши сотрудники</p>
+          <div className="block-2">
+            <div className="narrow-block light block-2-first">
+              <p>Наши сотрудники</p>
+            </div>
+            <div className="narrow-block light block-2-second">
+              <p>Просветляет понимающий тест</p>
+            </div>
+            <div className="narrow-block blue block-2-third">
+              <p>Интроекция</p>
+            </div>
+            <div className="wide-narrow-block light block-2-fouth">
+              <p>Конформизм</p>
+            </div>
+            <div className="narrow-block yellow block-2-fifth">
+              <p>Наши сотрудники</p>
+            </div>
           </div>
         </div>
         <div className="block-3">
@@ -46,19 +48,57 @@ function App() {
             <p>Понятие модернизации понимает механизм власти</p>
           </div>
           <div className="big-block green block-3-second">
-            <p>
-              Гений непредвзято понимает под собой конфликт. Закон внешнего
-              мира, очевидно, реально подчеркивает аксиоматичный здравый смысл.
-              Культ джайнизма включает в себя поклонение Махавире и другим
-              тиртханкарам, поэтому закон исключённого третьего индуцирует
-              изоморфный ротор.
-            </p>
+            <li>
+              <p>Фамилия</p>
+              <div></div>
+              <p>Мамай</p>
+            </li>
+            <li>
+              <p>Имя и отчество</p>
+              <div></div>
+              <p>
+                Станиславаленина<br></br> Владимировна
+              </p>
+            </li>
+            <li>
+              <p>
+                Номер полиса выдан заведомо<br></br> ложный
+              </p>
+              <div></div>
+              <p>01354879</p>
+            </li>
+            <li>
+              <p>Дата рождения</p>
+              <div></div>
+              <p>01.12.1976</p>
+            </li>
+            <li>
+              <p>Телефон</p>
+              <div></div>
+              <p>+79254218069</p>
+            </li>
           </div>
           <div className="small-block image block-3-third">
             <p>Наши сотрудники</p>
           </div>
         </div>
-        <div className="block-4">main content</div>
+        <div className="block-4">
+          <div className="narrow-block orandge block-4-first">
+            <p>Лидерство</p>
+          </div>
+          <div className="wide-narrow-block orandge block-4-second">
+            <p>Однако</p>
+          </div>
+          <div className="narrow-block red block-4-third">
+            <p>Невероятный гений</p>
+          </div>
+          <div className="narrow-block orandge block-4-fouth">
+            <p>Точка перегиба</p>
+          </div>
+          <div className="narrow-block perple block-4-fifth">
+            <p>Длина вектора</p>
+          </div>
+        </div>
       </div>
       <header className="App-header-footer">
         <p>footer</p>
